f3c1bb723cfdd5f6d37a3300690a3c9c9b90dcbf
rakhi6997 <ramakrishna060997@gmail.com>
2022-02-15 15:06:22 +0530

    Using arbitory values and Transpiling works

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,35 @@
 import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const App = () => {
+    const ref = useRef<any>();
     const [input,setInput] = useState('');
     const [code,setCode] = useState('');
 
     const startService = async () => {
-        const service = await esbuild.startService({
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: '/esbuild.wasm'
         });
-        console.log(service);
+        
     };
     useEffect(() => {
         startService();
     }, []);
     
-    const onClick = () => {
-        console.log(input);
-    }
+    const onClick = async () => {
+        if(!ref.current){
+            return;
+        }
+
+       const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target:'es2015'
+        });
+
+        setCode(result.code);
+    };
     
     return <div>
         <textarea value = {input} onChange = {(e)  => setInput(e.target.value)}></textarea>
